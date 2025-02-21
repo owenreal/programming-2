908abe6c37cc5ebe346e4a525405616bd826dedd
owenreal <162478543+owenreal@users.noreply.github.com>
2025-02-21 15:28:15 -0600

    2/21

---
+++ Prog122bFor/Prog122bFor/MainForm.py
@@ -0,0 +1,107 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._label1 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(13, 353)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(121, 53)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Show"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(191, 353)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(121, 53)
+		self._button2.TabIndex = 1
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(362, 353)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(121, 53)
+		self._button3.TabIndex = 2
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
+		# 
+		# listBox1
+		# 
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(13, 13)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(470, 251)
+		self._listBox1.TabIndex = 3
+		# 
+		# label1
+		# 
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(12, 267)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(471, 23)
+		self._label1.TabIndex = 4
+		self._label1.Text = "HOW MANY HOURS DID YOU WORK FOR 4$ AN HOUR"
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# textBox1
+		# 
+		self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.Location = System.Drawing.Point(191, 305)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(121, 26)
+		self._textBox1.TabIndex = 5
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(495, 418)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._listBox1)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Prog122bFor"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button1Click(self, sender, e):
+		self._listBox1.Items.Clear()
+		hours = int(self._textBox1.Text)
+		self._listBox1.Items.Add("Rate of pay if 4$ an HOUR...")
+		self._listBox1.Items.Add("Hours\tPay")
+		for num in range(0, hours+1):
+			pay = num * 4
+			self._listBox1.Items.Add(str(num) + "\t" + str(pay))
+
+	def Button2Click(self, sender, e):
+		self._listBox1.Items.Clear()
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
