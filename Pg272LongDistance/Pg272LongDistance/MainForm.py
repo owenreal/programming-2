a826608072d7c013465a62b85ffbec2ed7eae50c
owenreal <162478543+owenreal@users.noreply.github.com>
2025-03-04 15:28:07 -0600

    3/4

---
+++ Pg272LongDistance/Pg272LongDistance/MainForm.py
@@ -0,0 +1,67 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 13, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 292)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(113, 46)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = True
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 13, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(180, 292)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(113, 46)
+		self._button2.TabIndex = 1
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 13, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(341, 292)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(113, 46)
+		self._button3.TabIndex = 2
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(466, 350)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Pg272LongDistance"
+		self.ResumeLayout(False)
+
+	def Button1Click(self, sender, e):
+		pass
+
+	def Button2Click(self, sender, e):
+		pass
+
+	def Button3Click(self, sender, e):
+		pass
